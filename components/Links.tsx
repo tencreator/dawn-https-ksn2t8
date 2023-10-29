449ee4338fb28c423f4d13ab95b8513bdc71892e
TenCreator <lucakinsey@gmail.com>
2023-10-29 09:36:09 +0000

    Update Links.tsx

--- components/Links.tsx
+++ components/Links.tsx
@@ -11,19 +11,19 @@ interface link {
 }
 
 export default function RenderLinks(props: props) {
-  const RenderLink = ({id, url, name}: link) => {
-    return (
-      <Link key={id} href={url}>
-        {name}
-      </Link>
-    )
-  }
-  
   return (
     <div>
       {props.links.map((link: link[])=>{
   return <RenderLink id={link.id} name={link.name} url={link.url}/>
       })}
     </div>
+  )
+}
+
+const RenderLink = (props: link) => {
+  return (
+    <Link key={props.id} href={props.url}>
+      {props.name}
+    </Link>
   )
 }
